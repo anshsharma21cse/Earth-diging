9612232a99418da818513ddf7962066467b8a209
anshsharma21cse <anshsharma210504@gmail.com>
2025-10-05 03:38:27 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
+/** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
-  theme: { extend: {} },
-  plugins: [],
-}
+  theme: {
+    extend: {}
+  },
+  plugins: []
+};
