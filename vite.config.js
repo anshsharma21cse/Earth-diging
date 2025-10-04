5ffb7404dfda5bf2b8df886b68f6975b610efa0d
anshsharma21cse <anshsharma210504@gmail.com>
2025-10-05 03:38:02 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,11 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    exclude: ["three/examples/jsm/webgpu"],
+    exclude: ["three/examples/jsm/webgpu"] // prevent Vite trying to resolve missing webgpu
   },
+  build: {
+    commonjsOptions: {
+      ignoreTryCatch: false
+    }
+  }
 });
