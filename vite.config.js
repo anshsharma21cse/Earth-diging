04fbfbadbefae0dfa41e0fcba0b1b39d4661b952
anshsharma21cse <anshsharma210504@gmail.com>
2025-10-05 04:00:06 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
-// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    include: [], // don't pre-bundle anything
-    exclude: ["three"]
+    exclude: ['three/examples/jsm/webgpu'] // ignore optional WebGPU import
   },
   build: {
     commonjsOptions: {
